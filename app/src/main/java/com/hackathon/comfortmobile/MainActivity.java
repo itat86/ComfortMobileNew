3bb42a5ec65793f8a405b5068221a72bd7fefb3d
Tatjana Schindler <tatjana-schindler@gmx.de>
2018-02-02 00:32:16 +0100

    main drawer ergänzt

--- app/src/main/java/com/hackathon/comfortmobile/MainActivity.java
+++ app/src/main/java/com/hackathon/comfortmobile/MainActivity.java
@@ -23,14 +23,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,17 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_important) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_kb) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_vd) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_adresse) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_zusatz) {
 
         }
 
